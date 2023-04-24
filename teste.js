adcbe4e9d14c4793573499b1256a7d4cfcbd1861
andreperna <andreperna@gmail.com>
2023-04-24 16:07:11 -0300

    build(deps): update package-lock.json, package.json and teste.js

--- teste.js
+++ teste.js
@@ -1,9 +1,3 @@
-import operations from "./index.js"
+import {operations} from "npm-teste"
 
-import {operations as op} from "./index.js"
-
-console.log(op.multiplica2(6,6))
-
-import { MyMath } from "./index.js"
-
-console.log((new MyMath).soma(1,5))
+console.log(operations.subtrai2(25,20))
